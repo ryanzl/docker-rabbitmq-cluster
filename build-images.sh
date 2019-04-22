3a5dcf172d19eafe3a446eac196142ecdca94fec
ryanzl <killerggf@qq.com>
2019-04-22 15:43:43 +0800

    构建脚本

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,10 @@
 TAG="3.7.0"
 
-docker build -t bijukunjummen/rabbitmq-base:$TAG base
-docker build -t bijukunjummen/rabbitmq-server:$TAG server
+# docker build -t ryanzl2018/rabbitmq-base:$TAG base
+docker build -t ryanzl2018/rabbitmq-server:$TAG server
+
+docker tag ryanzl2018/rabbitmq-server:$TAG ryanzl2018/rabbitmq-server:$TAG
+
+#登录dockerhub
+#docker login
+docker push  ryanzl2018/rabbitmq-server
